cd23487091a450365697b419b05597e4991d9142
Glitch (bosaltyapi-devtr) <none>
2020-12-08 11:12:10 +0000

    💼✋ Checkpoint ./package.json:16568942/12850 ./komutlar/komutlar.js:16568942/188

--- komutlar/komutlar.js
+++ komutlar/komutlar.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
     try {
-        await message.channel.send(`Komutlar: \n${client.commands.map(props => `\`${props.help.name}\``).join(" | ")}`);
+        await message.channel.send(`Komutlar: -çal -durdur -ses -geç -çalan -sıra -devam`);
     } catch (e) {
         throw e;
     }
@@ -16,7 +16,6 @@ module.exports.conf = {
 };
 
 module.exports.help = {
-  name: 'komutlar',
-  description: 'Botta bulunan tüm komutları gösterir',
-  usage: 'komutlar'
+  name: 'komutlar'
+
 };
