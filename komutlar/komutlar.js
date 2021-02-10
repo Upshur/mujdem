1d82bbd7bb1cf439d985e43816c6b1250e74b0f6
Glitch (bosaltyapi-devtr) <none>
2021-02-10 14:20:27 +0000

    📇📼 Checkpoint ./komutlar/sunucubilgi.js:26755948/2430 ./komutlar/sunucu-bilgi.js:26755948/1828 ./komutlar/komutlar.js:26755948/370

--- komutlar/komutlar.js
+++ komutlar/komutlar.js
@@ -3,17 +3,18 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
   try {
     await message.channel.send(
-      `<a:emoji_13:807058166965731328>Komutlar<a:emoji_13:807058166965731328>
-<a:emoji_13:807058166965731328>+çal<a:emoji_13:807058166965731328> 
-<a:emoji_13:807058166965731328>+durdur<a:emoji_13:807058166965731328> 
-<a:emoji_13:807058166965731328>+ses<a:emoji_13:807058166965731328> 
-<a:emoji_13:807058166965731328>+geç<a:emoji_13:807058166965731328>
-<a:emoji_13:807058166965731328>+çalan<a:emoji_13:807058166965731328>
-<a:emoji_13:807058166965731328>+sıra<a:emoji_13:807058166965731328>
-<a:emoji_13:807058166965731328>+devam<a:emoji_13:807058166965731328>`
+     `<a:emoji_13:807058166965731328>么Komutlar么<a:emoji_13:807058166965731328>
+ 
+ 
+<a:emoji_13:807058166965731328>么+çal么<a:emoji_13:807058166965731328> 
+<a:emoji_13:807058166965731328>么+durdur么<a:emoji_13:807058166965731328> 
+<a:emoji_13:807058166965731328>么+ses么<a:emoji_13:807058166965731328> 
+<a:emoji_13:807058166965731328>么+geç么<a:emoji_13:807058166965731328>
+<a:emoji_13:807058166965731328>么+çalan么<a:emoji_13:807058166965731328>
+<a:emoji_13:807058166965731328>么+sıra么<a:emoji_13:807058166965731328>
+<a:emoji_13:807058166965731328>么+devam么<a:emoji_13:807058166965731328>`
       
-  
-      
+    
       
       
       
@@ -36,7 +37,7 @@ module.exports.run = async (client, message, args) => {
       
       
       
-      
+    
     );
   } catch (e) {
     throw e;
