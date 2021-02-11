7e879b36b17a17c3aed89191caee404d5610f86e
Glitch (bosaltyapi-devtr) <none>
2021-02-11 22:45:06 +0000

    🚀🚔 Checkpoint ./komutlar/yardım.js:26755948/115 ./komutlar/bağış.js:26755948/625

---
+++ komutlar/bağış.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+//OTTOMAN CODE
+exports.run = async (client, message, params, args) => {
+//OTTOMAN CODE
+  const ottomanCODE = new Discord.RichEmbed()
+  .setColor(0xffff00)
+      .setAuthor(`bağış`)
+.addField(`**Bağış at**`, '**Alttaki barkod ile bize bağış yaparak destek olabilirsiniz:\n\nBARKOD: `barkodunuz/ ibanınız`**')      
+  return message.channel.sendEmbed(ottomanCODE);
+};
+//OTTOMAN CODE  
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+  };
+  //OTTOMAN CODE
+  exports.help = {
+    name: 'bağış',
+    description: 'bağış',
+    usage: 'bağış'
+  };
+ 
