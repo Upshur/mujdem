66d142c5ae7b7e695de9fd7d2828f39fa33174da
Glitch (bosaltyapi-devtr) <none>
2021-02-11 17:30:47 +0000

    💷👘 Checkpoint ./komutlar/ping.js:26755948/862 ./komutlar/avatar.js:26755948/545

---
+++ komutlar/avatar.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js')
+//OTTOMAN CODE
+exports.run = (client, message) => {
+const ottoman = message.mentions.users.first() || message.author
+const ottomancode = new Discord.RichEmbed()
+.setImage(ottoman.avatarURL)
+.setFooter('OttomaN Code - Avatar Kodu')
+.setColor('Random)
+message.channel.send(ottomancode)
+}
+//OTTOMAN CODE
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+  aliases: ['pp'],
+    permLevel: 0
+  };
+  //OTTOMAN CODE
+  exports.help = {
+    name: 'avatar',
+    description: 'avatar',
+    usage: 'avatar'
+  };
