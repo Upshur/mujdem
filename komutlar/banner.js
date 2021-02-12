93302831312e5e08eddf6f42a7cf9f822718f74a
Glitch (bosaltyapi-devtr) <none>
2021-02-12 16:56:48 +0000

    🏥🐞 Checkpoint ./komutlar/155.js:26755948/571 ./komutlar/yardım.js:26755948/164 ./komutlar/banner.js:26755948/672

---
+++ komutlar/banner.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+
+exports.run = async (client, message, args) => {
+  const yazi = args.slice(0).join('+'); 
+
+  if(!yazi) return message.channel.send(`**Lütfen yazı yazın!**`)
+  const linqo = `https://dummyimage.com/2000x500/33363c/ffffff&text=${yazi}`
+  .replace(' ', '+')
+
+  
+  const embed = new Discord.RichEmbed()
+  .setTitle("Buyur Bannerın ne yapıcaksan")
+  .setColor("RANDOM")
+  .setImage(linqo)
+  message.channel.send(embed)
+}
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+}
+
+exports.help = {
+    name: 'banner',
+    description: 'Yazdığınız yazıyı bannera çevirir.',
+    usage: 'banner <yazı>'
+}
